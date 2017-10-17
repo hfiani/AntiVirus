fb5491cd140cc7c8094bd8d9b367336d4f40c8fe
xserenz <xserenz@gmail.com>
2017-10-17 15:19:40 +0200

    projectile

---
+++ AntiVirus/Assets/_SCRIPTS/Projectile.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+
+	#region public variables
+	public float speed = 10.0f;
+	public float startSize = 1.0f;
+	public float endSize = 1.0f;
+	public AnimationCurve sizeCurve;
+	public float growthTime = 2.0f;
+	#endregion
+
+	#region private variables
+	private float timer;
+	private bool isAlive = true;
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+
+		timer = Time.time;
+
+		transform.localScale = new Vector3 (1, 1, 1) * startSize;
+
+		GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		updateSize ();
+
+		
+	}
+
+	void updateSize(){
+
+		float scale = Mathf.Lerp (startSize,endSize,sizeCurve.Evaluate ((Time.time - timer) / growthTime)); 
+		//Debug.Log (scale);
+
+		transform.localScale  = new Vector3 (scale,scale,scale);
+
+
+	}
+
+	void OnTriggerEnter(Collider col){
+
+		if (col.gameObject.GetComponent<InfectionRaycast> () != null && isAlive)
+		{
+			col.gameObject.GetComponent<InfectionRaycast> ().CreateImmunity(col.transform);
+
+			isAlive = false;
+
+			Destroy (gameObject);
+		}
+	}
+}
