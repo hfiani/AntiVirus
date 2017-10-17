e7f03e0548dd17b4ba0effd61c6d7b90243f6495
xserenz <xserenz@gmail.com>
2017-10-17 14:02:57 +0200

    dirty reboot

---
+++ AntiVirus/Assets/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+
+	#region public variables
+	public float speed = 10.0f;
+	#endregion
+
+	#region private variables
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ AntiVirus/Assets/_SCRIPTS/PlayerManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+
+	#region public variables
+	public Transform _ProjectileSpawnPoint = null;
+	public GameObject _ProjectilePrefab = null;
+
+	public float _FireDelay = 0.2f;
+
+	#endregion
+
+	#region private variables
+	private bool isShooting = false;
+	private float timer;
+
+	#endregion
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton (0) && Time.time - timer > _FireDelay) {
+
+			Shoot ();
+		}
+
+
+
+	}
+
+	void Shoot(){
+
+		isShooting = true;
+
+		timer = Time.time;
+
+		Instantiate (_ProjectilePrefab, _ProjectileSpawnPoint.position, _ProjectileSpawnPoint.rotation);
+
+	}
+
+
+}
