de1402c07bd3f4fd771ad51a8eb474294e4c0b7a
Hanna <Hanna>
2017-10-18 11:51:27 +0200

    Repairing blocks

--- AntiVirus/Assets/_SCRIPTS/InfectionRaycast.cs
+++ AntiVirus/Assets/_SCRIPTS/InfectionRaycast.cs
@@ -19,11 +19,13 @@ public class InfectionRaycast : MonoBehaviour
 	public float timeBetweenInfections = 0.5f;
 
 	public float timeToRemoveImmunity = 20.0f;
+	public float timeToRemoveRepairImmunity = 3.0f;
 
-	public float factorRemoveInfection = 0.6f;
+	public float factorRemoveInfection = 0.2f;
 
 	public bool infected = false;
 	public bool immune = false;
+	public bool repair_immune = false;
 	public bool repairing = false;
 	public VirusType virusType = VirusType.ALL_AT_ONCE;
 	public BlockType blockType = BlockType.DESTRUCTABLE_INFECTABLE;
@@ -31,8 +33,7 @@ public class InfectionRaycast : MonoBehaviour
 	#endregion
 
 	#region private variables
-	private float timeToRemoveInfection;
-	private float timeToReparation;
+	private Material original_material;
 	private float timeBetweenReparations;
 	private DateTime reparation_time = new DateTime(0);
 	private DateTime infection_time = new DateTime(0);
@@ -45,8 +46,6 @@ public class InfectionRaycast : MonoBehaviour
 		Vector3.left, Vector3.right,
 		Vector3.forward, Vector3.back
 	};
-	private int material_index_infected = 0;
-	private int material_index_repaired = 0;
 	private MaterialPropertyBlock matBlock;
 	#endregion
 
@@ -59,9 +58,8 @@ public class InfectionRaycast : MonoBehaviour
 		timeToInfection *= 1000;
 		timeBetweenInfections *= 1000;
 		timeToRemoveImmunity *= 1000;
+		timeToRemoveRepairImmunity *= 1000;
 
-		timeToRemoveInfection = timeToDestruction * factorRemoveInfection;
-		timeToReparation = timeToInfection * factorRemoveInfection;
 		timeBetweenReparations = timeBetweenInfections * factorRemoveInfection;
 
 		/*object[] materials_infection_object = Resources.LoadAll("Materials/Infections", typeof(Material));
@@ -70,6 +68,8 @@ public class InfectionRaycast : MonoBehaviour
 		{
 			materialsInfection [i] = (Material)materials_infection_object [i];
 		}*/
+
+		original_material = GetMaterial();
 
 		if (blockType == BlockType.UNDESTRUCTABLE_UNINFECTABLE)
 		{
@@ -80,28 +80,36 @@ public class InfectionRaycast : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		//to be removed
 		if (repairing && reparation_time == new DateTime (0))
 		{
 			RepairInfection ();
 		}
-		if (infected)
+
+		if (repairing)
+		{
+			float duration_repaired = (float)(DateTime.Now - reparation_time).TotalMilliseconds;
+
+			RepairNeighbours (duration_repaired);
+		}
+		else if (infected)
 		{
 			float duration_infected = (float)(DateTime.Now - infection_time).TotalMilliseconds;
 
 			ManageSelfInfection (duration_infected);
 			InfectNeighbours (duration_infected);
 		}
-		else if (repairing)
-		{
-			float duration_repaired = (float)(DateTime.Now - reparation_time).TotalMilliseconds;
 
-			ManageSelfReparation (duration_repaired);
-			RepairNeighbours (duration_repaired);
-		}
-		else
+		if (immune)
 		{
 			float duration_immuned = (float)(DateTime.Now - immunity_time).TotalMilliseconds;
-			CheckImmunity (duration_immuned);
+			CheckImmunity (duration_immuned, timeToRemoveImmunity);
+		}
+
+		if (repair_immune)
+		{
+			float duration_repair_immuned = (float)(DateTime.Now - reparation_time).TotalMilliseconds;
+			CheckImmunity (duration_repair_immuned, timeToRemoveRepairImmunity);
 		}
 	}
 	#endregion
@@ -111,9 +119,9 @@ public class InfectionRaycast : MonoBehaviour
 	/// Checks the immunity.
 	/// </summary>
 	/// <param name="duration_immuned">Duration immuned.</param>
-	void CheckImmunity(float duration_immuned)
+	void CheckImmunity(float duration_immuned, float time_to_remove_immunity)
 	{
-		if (immune && duration_immuned >= timeToRemoveImmunity)
+		if (duration_immuned >= time_to_remove_immunity)
 		{
 			RemoveImmunity ();
 		}
@@ -148,7 +156,7 @@ public class InfectionRaycast : MonoBehaviour
 	/// <param name="mat">Mat.</param>
 	void ChangeMaterial(Material mat)
 	{
-		
+
 		if (transform.GetComponent<MeshRenderer> () != null)
 		{
 			transform.gameObject.GetComponent<MeshRenderer> ().material = mat;
@@ -156,11 +164,30 @@ public class InfectionRaycast : MonoBehaviour
 		for (int i = 0; i < transform.childCount; i++)
 		{
 			GameObject child = transform.GetChild (i).gameObject;
-			if (child.GetComponent<MeshRenderer> ())
+			if (child.GetComponent<MeshRenderer> () != null)
 			{
 				child.GetComponent<MeshRenderer> ().material = mat;
 			}
 		}
+	}
+
+	/// <summary>
+	/// Gets the material.
+	/// </summary>
+	/// <returns>The material.</returns>
+	Material GetMaterial()
+	{
+		if (transform.GetComponent<MeshRenderer> () != null)
+		{
+			return transform.gameObject.GetComponent<MeshRenderer> ().material;
+		}
+
+		GameObject child = transform.GetChild (0).gameObject;
+		if (child.GetComponent<MeshRenderer> () != null)
+		{
+			return child.GetComponent<MeshRenderer> ().material;
+		}
+		return null;
 	}
 
 	/// <summary>
@@ -190,8 +217,7 @@ public class InfectionRaycast : MonoBehaviour
 		}*/
 		else
 		{
-			material_index_infected = (int)Math.Floor (duration_infected * materialsInfection.Length / timeToDestruction);
-			int material_index = material_index_infected + material_index_repaired;
+			int material_index = (int)Math.Floor (duration_infected * materialsInfection.Length / timeToDestruction);
 			if (material_index < materialsInfection.Length && material_index >= 0)
 			{
 				ChangeMaterial (materialsInfection [material_index]);
@@ -234,50 +260,36 @@ public class InfectionRaycast : MonoBehaviour
 	}
 
 	/// <summary>
-	/// Manages the infection of the current Block.
+	/// Repairs the neighbours
 	/// </summary>
-	/// <param name="duration_infected">Duration infected.</param>
-	void ManageSelfReparation(float duration_repaired)
-	{
-		if (duration_repaired >= timeToRemoveInfection) // time's up -> destroy object
-		{
-			RemoveInfection ();
-		}
-		else
-		{
-			material_index_repaired = (int)Math.Floor (duration_repaired * materialsInfection.Length / timeToRemoveInfection);
-			int material_index = material_index_infected + material_index_repaired;
-			if (material_index < materialsInfection.Length && material_index >= 0)
-			{
-				ChangeMaterial (materialsInfection [materialsInfection.Length - 1 - material_index]);
-			}
-		}
-	}
-
-	/// <summary>
-	/// Infects the neighbours
-	/// </summary>
-	/// <param name="duration_infected">Duration infected.</param>
+	/// <param name="duration_infected">Duration Repaired.</param>
 	void RepairNeighbours(float duration_repaired)
 	{
 		if (virusType == VirusType.ALL_AT_ONCE) // parallel infections
 		{
-			if (duration_repaired >= timeToReparation)
+			foreach (Vector3 direction in directions)
 			{
-				foreach (Vector3 direction in directions)
+				Transform t = GetNeighbourBlocks (direction, distance);
+				if (t != null && t.gameObject.GetComponent<InfectionRaycast> () != null)
 				{
-					Transform t = GetNeighbourBlocks (direction, distance);
-					t.gameObject.GetComponent<InfectionRaycast>().RepairInfection ();
+					t.gameObject.GetComponent<InfectionRaycast> ().RepairInfection ();
 				}
 			}
 		}
 		else if (virusType == VirusType.ONE_AT_TIME) // serial infections
 		{
-			if (duration_repaired >= timeToReparation + timeBetweenReparations * blockTurn && blockTurn < directions.Length)
+			if (duration_repaired >= timeBetweenReparations * blockTurn && blockTurn < directions.Length)
 			{
 				Transform t = GetNeighbourBlocks (directions [blockTurn], distance);
-				t.gameObject.GetComponent<InfectionRaycast>().RepairInfection ();
+				if (t != null && t.gameObject.GetComponent<InfectionRaycast> () != null)
+				{
+					t.gameObject.GetComponent<InfectionRaycast> ().RepairInfection ();
+				}
 				blockTurn++;
+			}
+			else if (blockTurn >= directions.Length)
+			{
+				repairing = false;
 			}
 		}
 	}
@@ -316,13 +328,10 @@ public class InfectionRaycast : MonoBehaviour
 	/// </summary>
 	public void CreateInfection()
 	{
-		if (blockType != BlockType.UNDESTRUCTABLE_UNINFECTABLE && !immune)
+		if (blockType != BlockType.UNDESTRUCTABLE_UNINFECTABLE && !immune && !repair_immune && !infected)
 		{
 			infection_time = DateTime.Now;
 			infected = true;
-			material_index_repaired += material_index_infected;
-			material_index_infected = 0;
-
 		}
 	}
 
@@ -331,13 +340,12 @@ public class InfectionRaycast : MonoBehaviour
 	/// </summary>
 	public void RemoveInfection()
 	{
-		if (blockType != BlockType.UNDESTRUCTABLE_UNINFECTABLE)
+		if (blockType != BlockType.UNDESTRUCTABLE_UNINFECTABLE && infected)
 		{
 			infected = false;
-			ChangeMaterial(Resources.Load<Material>("Materials/mat_cube"));
+			ChangeMaterial(original_material);
+			infection_time = new DateTime (0);
 			blockTurn = 0;
-			material_index_infected = 0;
-			material_index_repaired = 0;
 		}
 	}
 
@@ -346,11 +354,12 @@ public class InfectionRaycast : MonoBehaviour
 	/// </summary>
 	public void CreateImmunity()
 	{
-		if (blockType != BlockType.UNDESTRUCTABLE_UNINFECTABLE && !immune)
+		if (blockType != BlockType.UNDESTRUCTABLE_UNINFECTABLE)
 		{
 			RemoveInfection();
 
 			immune = true;
+			repair_immune = false;
 			ChangeMaterial(Resources.Load<Material>("Materials/mat_cube - immune"));
 			immunity_time = DateTime.Now;
 		}
@@ -364,7 +373,8 @@ public class InfectionRaycast : MonoBehaviour
 		if (blockType != BlockType.UNDESTRUCTABLE_UNINFECTABLE && immune)
 		{
 			immune = false;
-			ChangeMaterial(Resources.Load<Material>("Materials/mat_cube"));
+			repair_immune = false;
+			ChangeMaterial(original_material);
 			immunity_time = new DateTime (0);
 			blockTurn = 0;
 		}
@@ -377,11 +387,12 @@ public class InfectionRaycast : MonoBehaviour
 	{
 		if (blockType != BlockType.UNDESTRUCTABLE_UNINFECTABLE && infected)
 		{
-			material_index_infected += material_index_repaired;
-			material_index_repaired = 0;
+			RemoveInfection ();
+
 			immune = false;
 			infected = false;
 			repairing = true;
+			repair_immune = true;
 			reparation_time = DateTime.Now;
 			blockTurn = 0;
 		}
